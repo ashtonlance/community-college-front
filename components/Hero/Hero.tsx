8a2a83f8b4756956cc0009f388ff509f5991b8b9
branquinhoa <amanda.alves15@gmail.com>
2023-09-22 09:18:39 -0300

    add bg video as an option for landing heros

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -2,27 +2,33 @@ import { DefaultHero } from './DefaultHero'
 import { LandingHero } from './LandingHero'
 import { ScrollIndicator } from './ScrollIndicator'
 
+
 export type heroTypeProp = 'landing' | 'default'
 
 type HeroAttributesProps = {
   attributes: {
     data: {
       hero_design: heroTypeProp
-      background_color: string
-      background_image: string
-      cta_button_label: string
-      cta_button_link: string
+      background_color?: string
+      background_image?: string
+      background_video_url?: string
+      cta_button_label?: string
+      cta_button_link?: string
       heading: string
       description: string
-      background_image_position: string
+      background_image_position?: string
+      background_video_file_or_url?:string
+      background_video_uploaded?:number
     }
   }
 }
 
 export const Hero = ({ attributes }: HeroAttributesProps) => {
   const heroType = attributes.data.hero_design
-  const bgColor = attributes.data.background_color || 'emerald'
+  const bgColor = attributes.data.background_color
   const bgImg = attributes.data.background_image
+  let bgVideo = null
+  const videoType = attributes?.data?.background_video_file_or_url
   const ctaLabel = attributes.data.cta_button_label
   const ctaURL = attributes.data.cta_button_link
   const heading = attributes.data.heading
@@ -30,15 +36,23 @@ export const Hero = ({ attributes }: HeroAttributesProps) => {
   const description = attributes.data.description
   const position = attributes.data.background_image_position
 
+  if(videoType == 'file') {
+    bgVideo = attributes?.data?.background_video_uploaded;
+  } else if (videoType == 'url') {
+    bgVideo = attributes?.data?.background_video_url;
+  }
+
   switch (heroType) {
     case 'landing':
-      const emptyBg = !bgImg && (!bgColor || bgColor=='grey');
+      const emptyBg = !bgImg && (!bgColor || bgColor=='grey') && !bgVideo;
 
       return (
-        <div className="relative landing-hero-wrapper">
+        <div className="relative landing-hero-wrapper overflow-hidden">
           <LandingHero
             bgColor={bgColor}
             bgImg={bgImg}
+            bgVideo={bgVideo}
+            videoType = {videoType}
             subheading={subheading}
             description={description}
             heading={heading}
