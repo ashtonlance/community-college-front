bf9e8ff96a669268ed3b32d05fbf98aa5d6789f6
branquinhoa <amanda.alves15@gmail.com>
2023-09-28 18:31:34 -0300

    add staff template page

--- components/PostFilter/PostFilterNumberedMemos.tsx
+++ components/PostFilter/PostFilterNumberedMemos.tsx
@@ -1,52 +1,68 @@
 export const PostFilterNumberedMemos = ({
   filters,
   setFilters,
-  categories,
-  years,
+  filtersToGenerateDropdown,
 }) => {
+  console.log({ filtersToGenerateDropdown })
+
   return (
-    <div className="flex justify-center md:justify-between md:gap-y-[15px] md:flex-wrap gap-x-[15px] px-[205px] py-[40px] md:px-[60px] mdsm:px-[40px] mdsm:py-[10px] mdsm:flex-col mdsm:gap-y-[10px] mdsm:w-full mdsm:mb-[32px]">
-      <select
-        className="flex-1 md:flex-initial text-darkBeige py-[14px] px-[20px] mdsm:px-[14px] mdsm:py-[12px] md:w-[48%] mdsm:w-full body-regular"
-        onChange={e => setFilters({ ...filters, county: e.target.value })}
-      >
-        <option value="">Select a category</option>
-        {categories.map(category => (
-          <option key={category} value={category}>
-            {category}
-          </option>
-        ))}
-      </select>
-      <select
-        className="flex-1 md:flex-initial text-darkBeige py-[14px] px-[20px] mdsm:px-[14px] mdsm:py-[12px] md:w-[48%] mdsm:w-full body-regular"
-        onChange={e => setFilters({ ...filters, year: e.target.value })}
-      >
-        <option value="">Select a Year</option>
-        {years.map(year => (
-          <option key={year} value={year}>
-            {year}
-          </option>
-        ))}
-      </select>
-      <input
-        className="text-input py-[14px] px-[20px] mdsm:px-[14px] mdsm:py-[12px] md:w-[48%] mdsm:w-full body-regular"
-        type="text"
-        placeholder="Search by keyword"
-        onChange={e => setFilters({ ...filters, keyword: e.target.value })}
-      />
-      <select
-        onChange={e =>
-          setFilters({
-            ...filters,
-            orderBy: { field: 'DATE', order: e.target.value },
-          })
-        }
-        className="flex-1 md:flex-initial text-navy py-[14px] px-[20px] mdsm:px-[14px] mdsm:py-[12px] md:w-[48%] mdsm:w-full body-regular"
-      >
-        <option value="ASC">Order by</option>
-        <option value="ASC">Date Ascending</option>
-        <option value="DESC">Date Descending</option>
-      </select>
+    <div className="mdsm:px-[40px] mdsm:py-[10px] mdsm:flex-col mdsm:gap-y-[10px] mdsm:w-full mdsm:mb-[32px] flex justify-center gap-x-[15px] px-[205px] py-[40px] md:flex-wrap md:justify-between md:gap-y-[15px] md:px-[60px]">
+      {filtersToGenerateDropdown.map(
+        filterOption =>
+          filterOption.type == 'select' &&
+          filterOption.name !== 'sort by' && (
+            <select
+              key={filterOption.name}
+              className="mdsm:px-[14px] mdsm:py-[12px] mdsm:w-full body-regular flex-1 px-[20px] py-[14px] text-darkBeige md:w-[48%] md:flex-initial"
+              onChange={e => setFilters({ ...filters, county: e.target.value })}
+            >
+              <option className="capitalize" value="">
+                Select a {filterOption.name}
+              </option>
+              {filterOption.options.map(option => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          )
+      )}
+
+      {filtersToGenerateDropdown.map(
+        filterOption =>
+          filterOption.type == 'select' &&
+          filterOption.name == 'sort by' && (
+            <select
+              key={filterOption.name}
+              className="mdsm:px-[14px] mdsm:py-[12px] mdsm:w-full body-regular flex-1 px-[20px] py-[14px] text-darkBeige md:w-[48%] md:flex-initial"
+              onChange={e =>
+                setFilters({
+                  ...filters,
+                  orderBy: { field: 'NAME', order: e.target.value },
+                })
+              }
+            >
+              <option value="ASC">Order by</option>
+              <option value="ASC">Date Ascending</option>
+              <option value="DESC">Date Descending</option>
+            </select>
+          )
+      )}
+
+      {filtersToGenerateDropdown.map(
+        filterOption =>
+          filterOption.type == 'input' && (
+            <input
+              key={filterOption}
+              className="text-input mdsm:px-[14px] mdsm:py-[12px] mdsm:w-full body-regular px-[20px] py-[14px] md:w-[48%]"
+              type="text"
+              placeholder="Search by keyword"
+              onChange={e =>
+                setFilters({ ...filters, keyword: e.target.value })
+              }
+            />
+          )
+      )}
     </div>
   )
 }
---
+++ wp-templates/page-staff.tsx
@@ -0,0 +1,238 @@
+import { gql } from '@apollo/client'
+import { WordPressBlocksViewer } from '@faustwp/blocks'
+import { PreFooter } from 'components/PreFooter'
+import { Layout } from 'components/Layout'
+import { flatListToHierarchical } from 'utils/flatListToHierarchical'
+import { PostFilterNumberedMemos } from '@/components/PostFilter'
+import { useEffect, useMemo, useState, useCallback } from 'react'
+import { useDebounce } from '@uidotdev/usehooks'
+import { useRouter } from 'next/router'
+import { PaginatedPosts } from '@/components/PaginatedPosts'
+
+export default function StaffIndexPage({ data, loading, error }) {
+  const router = useRouter()
+  const { page } = router.query
+  const currentPage = parseInt((Array.isArray(page) ? page[0] : page) || '1')
+
+  const menuItems = data?.menu?.menuItems || []
+  const pageData = data?.page
+  const preFooterContent = data?.menus?.nodes[0]
+  const blocks = pageData && [...pageData.blocks]
+  const utilityNavigation = data?.settings?.utilityNavigation?.navigationItems
+  const hierarchicalMenuItems = flatListToHierarchical(menuItems as any) || []
+  const footerMenuItems = data?.footer?.menuItems || []
+  const hierarchicalFooterMenuItems =
+    flatListToHierarchical(footerMenuItems as any) || []
+  const settings = data?.settings?.siteSettings || []
+
+  console.log({ data })
+
+  const staffIndex = useMemo(
+    () => data?.allStaff?.nodes || [],
+    [data?.allStaff?.nodes]
+  )
+
+  const [filters, setFilters] = useState({
+    organization: '',
+    department: '',
+    orderBy: { field: 'Last Name', order: 'ASC' },
+  })
+
+  const organizations = useMemo(
+    () => [
+      ...new Set(
+        staffIndex?.flatMap(staff =>
+          staff.staffDetails.organizations.flatMap(org => org.name)
+        )
+      ),
+    ],
+    [staffIndex]
+  )
+  const departments = useMemo(
+    () => [...new Set(staffIndex.map(staff => staff.staffDetails.location))],
+    [staffIndex]
+  )
+
+  const debouncedFilters = useDebounce(filters, 500)
+  const [filteredMemos, setFilteredMemos] = useState(staffIndex)
+
+  const filterNumberedMemos = useCallback(() => {
+    let result = staffIndex
+
+    if (debouncedFilters.organization) {
+      result = result.filter(memo =>
+        memo.numberedMemoCategories.nodes.name
+          .toLowerCase()
+          .includes(debouncedFilters.organization.toLowerCase())
+      )
+    }
+
+    if (debouncedFilters.department) {
+      result = result.filter(memo =>
+        memo.numberedMemo.date.includes(debouncedFilters.department)
+      )
+    }
+
+    // if (debouncedFilters.orderBy.order === 'DESC') {
+    //   result = result.sort(
+    //     (a, b) => b.numberedMemo?.date?.localeCompare(a.numberedMemo?.date)
+    //   )
+    // } else {
+    //   result = result.sort(
+    //     (a, b) => a.numberedMemo?.date?.localeCompare(b.numberedMem?.date)
+    //   )
+    // }
+
+    setFilteredMemos(result)
+  }, [
+    debouncedFilters.organization,
+    debouncedFilters.department,
+    debouncedFilters.orderBy.order,
+    staffIndex,
+  ])
+
+  useEffect(() => {
+    filterNumberedMemos()
+  }, [debouncedFilters, filterNumberedMemos])
+
+  if (loading) {
+    return <>Loading...</>
+  }
+
+  const filtersToGenerateDropdown = [
+    {
+      name: 'organization',
+      options: organizations,
+      type: 'select'
+    },
+    {
+      name: 'department',
+      options: departments,
+      type: 'select'
+    },
+    {
+      name: 'sort by',
+      options: 'Last Name',
+      type: 'select'
+    },
+  ]
+
+  return (
+    <Layout
+      menuItems={hierarchicalMenuItems}
+      seo={pageData?.seo}
+      headerVariant={'default'}
+      utilityNavigation={utilityNavigation}
+      footerNavigation={hierarchicalFooterMenuItems}
+      settings={settings}
+    >
+      <div className="h-full bg-grey">
+        {blocks && (
+          <WordPressBlocksViewer fallbackBlock={[] as any} blocks={blocks} />
+        )}
+        <PostFilterNumberedMemos
+          filters={filters}
+          setFilters={setFilters}
+          filtersToGenerateDropdown={filtersToGenerateDropdown}
+          //   categories={organizations}
+          //   years={departments}
+        />
+        <div className="index-page-wrapper bg-grey">
+          {/* <PaginatedPosts
+            currentPage={currentPage}
+            postType="numberedMemo"
+            posts={filteredMemos}
+          /> */}
+        </div>
+        {preFooterContent && <PreFooter preFooterContent={preFooterContent} />}
+      </div>
+    </Layout>
+  )
+}
+
+StaffIndexPage.variables = ({ uri }) => {
+  return { uri }
+}
+
+StaffIndexPage.query = gql`
+  query staffIndex($uri: ID!) {
+    page(id: $uri, idType: URI) {
+      id
+      slug
+      status
+      title
+      link
+      blocks
+      seo {
+        metaDesc
+        canonical
+        title
+        schema {
+          raw
+        }
+      }
+    }
+
+    allStaff(where: { orderby: { field: DATE, order: ASC } }) {
+      nodes {
+        id
+        staffDetails {
+          email
+          fieldGroupName
+          jobTitle
+          location
+          name
+          phone
+          location
+          organizations {
+            name
+            link
+          }
+        }
+      }
+    }
+
+    menu(id: "System Office", idType: NAME) {
+      menuItems(first: 200) {
+        nodes {
+          id
+          parentId
+          description
+          label
+          url
+        }
+      }
+    }
+    footer: menu(id: "Footer", idType: NAME) {
+      menuItems(first: 200) {
+        nodes {
+          id
+          parentId
+          description
+          label
+          url
+        }
+      }
+    }
+
+    settings {
+      siteSettings {
+        announcementBar {
+          announcementBarText
+          showAnnouncementBar
+          announcementBarLink
+        }
+      }
+
+      utilityNavigation {
+        navigationItems {
+          navItem {
+            title
+            url
+            target
+          }
+        }
+      }
+    }
+  }
+`
