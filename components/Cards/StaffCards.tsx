f63354673caa5809ca94430edb8f7568ebc1a0ba
Ashton Lance <ashton@simplefocus.com>
2023-10-16 14:23:25 -0400

    fix: Clickable phone numbers and emails

--- components/Cards/StaffCards.tsx
+++ components/Cards/StaffCards.tsx
@@ -15,32 +15,38 @@ export const StaffCardsHeading = () => {
 }
 
 export const StaffCards = ({ card }) => {
-  console.log({ card })
   const staffDetail = card.staffDetails
   return (
-    <div className="mdsm:grid-cols-4 mdsm:p-[20px] col-span-12 col-start-1 grid grid-flow-row grid-cols-12 items-center gap-[20px] rounded-[12px] bg-white px-[24px] py-[20px] md:grid-cols-6 md:px-[20px] md:py-[14px]">
-      <div className="footer-links-headline mdsm:col-span-4 col-span-3 col-start-1 mb-0 text-justify text-navy hover:text-navy md:col-span-2">
+    <div className="col-span-12 col-start-1 grid grid-flow-row grid-cols-12 items-center gap-[20px] rounded-[12px] bg-white px-[24px] py-[20px] md:grid-cols-6 md:px-[20px] md:py-[14px] mdsm:grid-cols-4 mdsm:p-[20px]">
+      <div className="footer-links-headline col-span-3 col-start-1 mb-0 text-justify text-navy hover:text-navy md:col-span-2 mdsm:col-span-4">
         {staffDetail.staffName}
       </div>
-      <div className="body-small mdsm:col-start-1 col-span-2 col-start-4 font-bold md:col-start-3">
+      <div className="body-small col-span-2 col-start-4 font-bold md:col-start-3 mdsm:col-start-1">
         {staffDetail.jobTitle}
       </div>
-      <div className="body-small mdsm:col-start-2 col-span-2 col-start-6 md:col-start-4">
-        {staffDetail.organizations.flatMap(org=>org.name)}
+      <div className="body-small col-span-2 col-start-6 md:col-start-4 mdsm:col-start-2">
+        {staffDetail.organizations.flatMap(org => org.name)}
       </div>
-      <div className="body-small mdsm:col-start-3 col-span-2 col-start-8 md:col-start-5">
+      <div className="body-small col-span-2 col-start-8 md:col-start-5 mdsm:col-start-3">
         {staffDetail.location}
       </div>
       <div className="col-span-2 col-start-10 md:hidden">
-        {staffDetail.phone}
+        <a
+          className="text-darkGrey hover:text-navy"
+          href={`tel:${staffDetail.phone}`}
+        >
+          {staffDetail.phone}
+        </a>
       </div>
-      <Link
-        className="body-small mdsm:col-start-1 mdsm:justify-start mdsm:col-span-4 group col-span-1 col-end-13 flex items-center justify-end gap-[12px] font-bold text-darkGrey hover:text-navy md:col-span-1 md:col-end-7 sm:text-[12px]"
-        href={staffDetail.email}
-      >
-        Email
-        <Email className="text-gold transition-colors duration-100 group-hover:text-navy" />
-      </Link>
+      <div className="body-small group col-span-1 col-end-13 md:col-span-1 md:col-end-7 mdsm:col-span-4 mdsm:col-start-1 mdsm:justify-start sm:text-[12px]">
+        <a
+          className="flex items-center justify-end gap-[12px] text-darkGrey hover:text-navy"
+          href={`mailto:${staffDetail.email}`}
+        >
+          Email
+          <Email className="text-gold transition-colors duration-100 group-hover:text-navy" />
+        </a>
+      </div>
     </div>
   )
 }
