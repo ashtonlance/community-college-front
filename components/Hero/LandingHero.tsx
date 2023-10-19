49ce3842709c91ab055542b29f1062fe090098fc
Josh <35382077+JBamheedor@users.noreply.github.com>
2023-10-19 17:50:28 -0500

    chore: add back hero overlay

--- components/Hero/LandingHero.tsx
+++ components/Hero/LandingHero.tsx
@@ -60,7 +60,7 @@ export const LandingHero = ({
           priority
         />
       )}
-      {/* <div className="img-landing-hero absolute bottom-0 left-0 right-0 top-0 w-full">
+      <div className="img-landing-hero absolute bottom-0 left-0 right-0 top-0 w-full">
         <Image
           src={angles.src}
           alt=""
@@ -68,7 +68,7 @@ export const LandingHero = ({
           className={`img-landing-hero z-[2] object-fill`}
           priority
         />
-      </div> */}
+      </div>
       <div className="landing-hero-text relative z-10 ml-[15%] mr-[30%] flex w-[55%] flex-col md:mx-[100px] md:w-full sm:mx-[40px]">
         {subheading && (
           <div className="flex items-center gap-[15px]">
