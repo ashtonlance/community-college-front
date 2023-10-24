d6bff48af12c9aeaa0721ba195a7fde2a652a972
Josh <35382077+JBamheedor@users.noreply.github.com>
2023-10-24 12:59:54 -0500

    fix: link placeholder

--- components/GravityForm/GravityFormsFields/TextAreaField.tsx
+++ components/GravityForm/GravityFormsFields/TextAreaField.tsx
@@ -9,18 +9,18 @@ export const TEXT_AREA_FIELD_FIELDS = gql`
     description
     cssClass
     isRequired
+    placeholder
   }
 `
 
 const DEFAULT_VALUE = ''
 
 export default function TextAreaField({ field, fieldErrors }) {
-  const { id, formId, type, label, description, cssClass, isRequired } = field
+  const { id, formId, type, label, description, cssClass, isRequired, placeholder } = field
   const htmlId = `field_${formId}_${id}`
   const { state, dispatch } = useGravityForm()
   const fieldValue = state.find(fieldValue => fieldValue.id === id)
   const value = fieldValue?.value || DEFAULT_VALUE
-
   return (
     <div className={`gfield  gfield-${type} ${cssClass} basis-full`.trim()}>
       <label htmlFor={htmlId} className="gfield_label">
@@ -29,7 +29,7 @@ export default function TextAreaField({ field, fieldErrors }) {
       </label>
       <textarea
         className="h-52 w-full text-navy"
-        placeholder={label || ''}
+        placeholder={placeholder || ''}
         name={String(id)}
         id={htmlId}
         required={Boolean(isRequired)}
