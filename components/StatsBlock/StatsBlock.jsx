90017a8eedd06a6fb012e621d51e92ca01018724
Josh <35382077+JBamheedor@users.noreply.github.com>
2023-11-08 15:28:34 -0600

    update: stats block styles

--- components/StatsBlock/StatsBlock.jsx
+++ components/StatsBlock/StatsBlock.jsx
@@ -10,7 +10,7 @@ export const StatsBlock = ({ attributes }) => {
   const splitTitle = title.split('-')
   return (
     <FadeIn>
-      <div className={cn(`bg-navy flex flex-col items-center justify-center gap-y-[50px] sm:gap-y-[10px] relative py-[80px] px-[150px] md:p-[60px] md:pb-[80px] sm:py-[50px] sm:px-[40px]`)}>
+      <div className={cn(`bg-navy flex flex-col items-center justify-center gap-y-[50px] sm:gap-y-[10px] relative py-[80px] pb-[100px] px-[150px] md:p-[60px] md:pb-[80px] sm:py-[50px] sm:px-[40px]`)}>
         <Image
           className="pointer-events-none object-fill object-center"
           src={bg}
@@ -31,26 +31,28 @@ export const StatsBlock = ({ attributes }) => {
               </div>
             </div>
           ) : (
-            <div className="h5 text-center text-[16px] font-bold leading-[150%] text-white sm:text-[14px]">
+            title && (
+              <div className="h5 text-center text-[16px] font-bold leading-[150%] text-white sm:text-[14px] sm:mb-[32px] md:mb-[40px] mb-[50px]">
               {title}
             </div>
+            )
           )}
           {copy && (
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center sm:mb-[32px] md:mb-[40px] mb-[50px]">
               <p className="body-large text-center text-white max-w-[730px]">{copy}</p>
             </div>
           )}
-          <div className="flex flex-wrap justify-center gap-[100px] sm:flex-col sm:gap-0">
+          <div className="flex flex-wrap md:justify-center justify-start gap-[100px] sm:flex-col sm:gap-[20px] md:gap-[40px]">
             {items > 0 &&
               [...Array(items).keys()].map(val => (
                 <div
                   key={val}
-                  className="flex flex-col items-center justify-center gap-[10px]"
+                  className="flex flex-1 flex-col items-center gap-[12px]"
                 >
-                  <h3 className="home-h1 mb-[20px] text-lightBlue font-condensed sm:mb-0">
+                  <h3 className="home-h1 sm:text-[76px] text-[92px] mb-[20px] text-lightBlue font-condensed sm:mb-0 mt-0">
                     {attributes.data[`stats_${val}_number`]}
                   </h3>
-                  <span className="text-white max-w-[300px] text-center font-bold text-[28px] leading-[110%] tracking-[-0.28px]">
+                  <span className="text-white max-w-[300px] text-center font-bold sm:text-[24px] text-[28px] leading-[110%] tracking-[-0.28px]">
                     {attributes.data[`stats_${val}_explainer_text`]}
                   </span>
                 </div>
