ca99c16401cba2aa1f3399f83a9f0d6352b54742
Josh <35382077+JBamheedor@users.noreply.github.com>
2023-11-14 14:41:51 -0600

    fix: testimonial

--- components/TestimonialSlider/TestimonialSlider.tsx
+++ components/TestimonialSlider/TestimonialSlider.tsx
@@ -31,7 +31,7 @@ export const TestimonialSlider = ({ attributes }: TestimonialSliderProps) => {
 
   const autoplay = useRef(
     Autoplay(
-      { delay: 7500, stopOnInteraction: false, jump: true},
+      { delay: 7500, stopOnInteraction: false},
       // @ts-expect-error
       embla => embla.parentElement
     )
@@ -62,7 +62,7 @@ export const TestimonialSlider = ({ attributes }: TestimonialSliderProps) => {
   const createCarouselItem = (val, options = {}) => (
     <div
       key={`slide${val}`}
-      className={`flex flex-shrink-0 flex-grow-0 basis-full flex-col items-center justify-center overflow-hidden`}
+      className={`flex flex-shrink-0 flex-grow-0 basis-full flex-col items-center justify-center overflow-hidden sm:p-[32px] md:px-[50px] px-[105px]`}
     >
       <Quote className="mb-[40px] text-gold" />
       <p className="h4 mb-10">{attributes.data[`slide_${val}_quote`]}</p>
@@ -95,7 +95,7 @@ export const TestimonialSlider = ({ attributes }: TestimonialSliderProps) => {
       >
 
         <div
-          className={`${bgColorCard == 'grey' ? "bg-white" : "bg-grey"} max-w-[1240px] mx-auto rounded-xl flex relative flex-col items-center justify-center px-[105px] py-[60px] text-center md:w-full sm:p-[32px] md:py-[40px] md:px-[50px]`}
+          className={`${bgColorCard == 'grey' ? "bg-white" : "bg-grey"} max-w-[1240px] mx-auto rounded-xl flex relative flex-col items-center justify-center py-[60px] text-center md:w-full sm:py-[32px] md:py-[40px]`}
         >
         <Image
           src={`${bgColorCard == 'grey' ? bgWhite.src : bgGrey.src}`}
