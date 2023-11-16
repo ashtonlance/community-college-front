2204bf04791ad3e7b6d7b77d54cdf2dbd7a07dd5
Josh <35382077+JBamheedor@users.noreply.github.com>
2023-11-16 13:27:36 -0600

    update: program areas angled bg

--- wp-templates/single-program-areas.tsx
+++ wp-templates/single-program-areas.tsx
@@ -5,6 +5,8 @@ import { Header } from 'components/Header'
 import { Layout } from 'components/Layout'
 import Link from 'next/link'
 import { flatListToHierarchical } from 'utils/flatListToHierarchical'
+import bgFlip from '/assets/imgs/angled-bg-defaultHero-flip.png'
+import Image from 'next/image'
 
 export default function SingleProgramArea(props) {
   const menuItems = props.data?.menu?.menuItems || []
@@ -42,47 +44,58 @@ export default function SingleProgramArea(props) {
       {pageData?.programAreaDetails?.about ? (
         <WYSIWYG
           attributes={{
-            data: { content: pageData?.programAreaDetails?.about },
+            data: { 
+              content: pageData?.programAreaDetails?.about,
+              background_color: 'grey'
+            },
           }}
         />
       ) : null}
       {relatedPrograms?.length ? (
-        <div className="p-[100px] md:p-[60px] sm:p-[40px] ">
-          <h2 className="h3 mb-10 flex items-center gap-3">
-            Programs in This Program Area{' '}
-            <span className="h5 text-darkBeige">{relatedPrograms?.length}</span>
-          </h2>
-          <ul className="grid grid-cols-5 gap-5 md:grid-cols-4 md:gap-[15px] sm:grid-cols-2 sm:gap-[10px]">
-            {relatedPrograms.map(program => {
-              return (
-                <div
-                  className="group relative flex w-full items-center justify-center overflow-hidden rounded-xl bg-grey p-5 hover:bg-white hover:outline hover:outline-2 hover:outline-lightBlue"
-                  key={program.uri}
-                >
-                  <Link
-                    className="text-center font-condensed text-xl font-bold tracking-[-0.2px] text-navy"
-                    href={program.uri}
+        <div className="relative">
+          <Image
+            src={bgFlip.src}
+            fill
+            alt="background"
+            className="object-fill h-auto z-[-1]"
+          />
+          <div className="p-[100px] md:p-[60px] sm:p-[40px] ">
+            <h2 className="h3 mb-10 flex items-center gap-3">
+              Programs in This Program Area{' '}
+              <span className="h5 text-darkBeige">{relatedPrograms?.length}</span>
+            </h2>
+            <ul className="grid grid-cols-5 gap-5 md:grid-cols-4 md:gap-[15px] sm:grid-cols-2 sm:gap-[10px]">
+              {relatedPrograms.map(program => {
+                return (
+                  <div
+                    className="group relative flex w-full items-center justify-center overflow-hidden rounded-xl bg-grey p-5 hover:bg-white hover:outline hover:outline-2 hover:outline-lightBlue"
+                    key={program.uri}
                   >
-                    {program.title}
-                  </Link>
-                  {(program.program.degreeTypes?.includes(
-                    'continuingEducation'
-                  ) ||
-                    program.program.degreeTypes?.includes(
-                      'workforceContinuingEducation'
-                    )) && (
-                    <div className="tag absolute right-0 top-0 rounded-bl-lg rounded-tr-lg bg-white text-darkGrey group-hover:bg-lightBlue">
-                      {program.program.degreeTypes?.includes(
-                        'continuingEducation'
-                      )
-                        ? 'WCE'
-                        : 'WCE'}
-                    </div>
-                  )}
-                </div>
-              )
-            })}
-          </ul>
+                    <Link
+                      className="text-center font-condensed text-xl font-bold tracking-[-0.2px] text-navy"
+                      href={program.uri}
+                    >
+                      {program.title}
+                    </Link>
+                    {(program.program.degreeTypes?.includes(
+                      'continuingEducation'
+                    ) ||
+                      program.program.degreeTypes?.includes(
+                        'workforceContinuingEducation'
+                      )) && (
+                      <div className="tag absolute right-0 top-0 rounded-bl-lg rounded-tr-lg bg-white text-darkGrey group-hover:bg-lightBlue">
+                        {program.program.degreeTypes?.includes(
+                          'continuingEducation'
+                        )
+                          ? 'WCE'
+                          : 'WCE'}
+                      </div>
+                    )}
+                  </div>
+                )
+              })}
+            </ul>
+          </div>
         </div>
       ) : null}
     </Layout>
