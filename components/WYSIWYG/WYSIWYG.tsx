66f0f175b00a9aad1fcb4264e0073485394464f7
Adam Gigstad <adam.gigstad@gmail.com>
2023-11-30 15:36:38 -0800

    Revert "Merge branch 'main' of github.com:simplefocus/ncccs"
    This reverts commit 55c6b3475d674a5ce6da6cba7a1c53659e832d42, reversing
    changes made to 820f53d2b1f0879582fd7175f1f1dbdc887e8bf4.

--- components/WYSIWYG/WYSIWYG.tsx
+++ components/WYSIWYG/WYSIWYG.tsx
@@ -1,6 +1,4 @@
 import { Parser } from 'html-to-react'
-import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
-import { createRoot } from 'react-dom/client'
 import { renderToString } from 'react-dom/server'
 
 type WYSIWYGProps = {
@@ -17,117 +15,19 @@ type WYSIWYGProps = {
 }
 
 export const WYSIWYG: React.FC<WYSIWYGProps> = props => {
-  const { customClasses = '', attributes } = props
-  const {
-    content = '',
-    background_color: bgColor = '',
-    component_spacing_bottom_spacing: bottomSpacing = 'medium',
-    component_spacing_top_spacing: topSpacing = 'medium',
-  } = attributes?.data || {}
+  const customClasses = props.customClasses || ''
+  const content = props?.attributes?.data?.content || ''
+  const bgColor = props?.attributes?.data?.background_color || ''
+  const bottomSpacing =
+    props?.attributes?.data?.component_spacing_bottom_spacing || 'medium'
+  const topSpacing =
+    props?.attributes?.data?.component_spacing_top_spacing || 'medium'
 
-  const parser = useMemo(() => new (Parser as any)(), [])
-  const reactElement = useMemo(() => parser.parse(content), [parser, content])
-  const reactHtml = useMemo(() => renderToString(reactElement), [reactElement])
-
-  const wysiwyg = useRef(null)
-  const mobile_table = useRef(null)
-
-  const [windowSize, setWindowSize] = useState({ width: undefined })
-
-  const handleResize = useCallback(() => {
-    const newWidth = window.innerWidth
-    if (windowSize.width !== newWidth) {
-      setWindowSize({ width: newWidth })
-    }
-  }, [windowSize.width])
-
-  useEffect(() => {
-    window.addEventListener('resize', handleResize)
-    handleResize()
-    if (windowSize.width < 640) {
-      initResponsiveTables()
-    }
-    return () => window.removeEventListener('resize', handleResize)
-  }, [windowSize.width, handleResize])
-
-  const setInnerTableData = useCallback(
-    html => <span dangerouslySetInnerHTML={{ __html: html }}></span>,
-    []
-  )
-
-  const MobileTable = useCallback(
-    ({ new_rows }) => <tbody ref={mobile_table}>{new_rows}</tbody>,
-    []
-  )
-
-  const initResponsiveTables = useCallback(() => {
-    const tables = wysiwyg.current.querySelectorAll('table')
-
-    if (tables) {
-      tables.forEach((table, tableIndex) => {
-        // Duplicate table on mobile to alter HTML
-        const table_clone = table.cloneNode(true)
-        const head = table.querySelectorAll('tr')[0]
-        // Allow for markup differences - tables use either th or td for headings
-        const headings =
-          head?.querySelectorAll('th').length > 0
-            ? head?.querySelectorAll('th')
-            : head?.querySelectorAll('td')
-        const rows = table.querySelectorAll('tr:not(:first-child)')
-        const new_rows = []
-
-        if (!mobile_table.current) {
-          table.after(table_clone)
-          table.classList.add('sm:hidden', 'table')
-          table_clone.classList.add('sm:table', 'hidden')
-        }
-
-        rows.forEach((row, rowIndex) => {
-          const cols = row.querySelectorAll('td')
-          const classname = rowIndex % 2 ? 'even' : 'odd'
-
-          cols.forEach((col, i) => {
-            const new_tr = React.createElement(
-              'tr',
-              {
-                className: classname,
-                key: `table-${tableIndex} row-${rowIndex}-${i}`,
-              },
-              React.createElement(
-                'td',
-                {
-                  className:
-                    'bg-lightBlue text-navy text-[12px] font-extrabold tracking-[0.72px] uppercase',
-                },
-                null,
-                headings[i]?.innerText
-              ),
-              React.createElement(
-                'td',
-                {
-                  className:
-                    'bg-transparent text-darkGrey text-[14px] sm:w-full w-auto',
-                },
-                null,
-                setInnerTableData(col.innerHTML)
-              )
-            )
-            new_rows.push(new_tr)
-          })
-        })
-
-        // If not already rendered
-        if (!mobile_table.current) {
-          const root = createRoot(table_clone)
-          root.render(<MobileTable new_rows={new_rows} />)
-        }
-      })
-    }
-  }, [setInnerTableData, MobileTable])
-
+  const parser = new (Parser as any)()
+  const reactElement = parser.parse(content)
+  const reactHtml = renderToString(reactElement)
   return (
     <div
-      ref={wysiwyg}
       className={`bg-${bgColor} ${customClasses} wysiwyg body-regular px-52 module-spacing-bottom-${bottomSpacing}  module-spacing-top-${topSpacing} md:px-[100px] md:py-[60px] sm:p-10`}
       dangerouslySetInnerHTML={{ __html: reactHtml }}
     />
